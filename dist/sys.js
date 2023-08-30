aba66e3e7b1b6d1c49ae67162f164ca38a8c5db7
Florian Utku <utflorian@gmail.com>
2023-08-30 12:18:57 +0200

    allow searching for r 75-75.4

--- dist/sys.js
+++ dist/sys.js
@@ -179,16 +179,22 @@ function search(searchString) {
   fuzzySearch = fuzzySearch.trim().toLowerCase();
 
   const inNotationBenennung = `//*[@Benennung[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZÜÖÄ', 'abcdefghijklmnopqrstuvwxyzüöä'), '${fuzzySearch}')]]`;
-  //const inNotationVerweisformen = `//*[@Verweisformen[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZÜÖÄ', 'abcdefghijklmnopqrstuvwxyzüöä'), '${fuzzySearch}')]]`;
   const forNotation = `//*[translate(name(), '_', ' ')='${searchString}']`;
   
   /** Search for a specific Notation or look for the searchString in "@Stichwort" */
-  const notationRegex = /^[a-z]{1,3} \d{1,3}(?:-\d{1,3}|.\d{1,3}.?\d?)?$/gmi;
+  const notationRegex = /^[a-z]{1,3} \d{1,3}(?:\.\d{1,3}){0,2}$/gmi;
+  const notationRangeRegex = /^[a-z]{1,3} \d{1,3}(?:\.\d{1,3}){0,2}-\d{1,3}(?:\.\d{1,3}){0,2}$/gmi;
   if (notationRegex.test(fuzzySearch)) {
-    searchRequest(forNotation, 'Notation');
+    searchRequest(forNotation, 'Notation'); // Search for a string that seems to be a singular notation
+  } else if (notationRangeRegex.test(fuzzySearch)) {
+    const parts = fuzzySearch.trim().replace(' ', '_').split('-');
+    const part1 = parts[0];
+    const part2 = `${parts[0].match(/^[a-z]{1,3}/gmi)[0]}_${parts[1]}`;
+    const notationRange = `//${part1} | //*[preceding::${part1}][following::${part2}] | //${part2}`;
+
+    searchRequest(notationRange, 'Notation'); // Search for a string that seems to be a range of notations, f.e. r 75-75.4
   } else {
-    searchRequest(inNotationBenennung, 'Notation');
-    //searchRequest(inNotationVerweisformen, 'Notation');
+    searchRequest(inNotationBenennung, 'Notation'); // Search for a string in the attribute "Benennung"
   }
 
   /**
